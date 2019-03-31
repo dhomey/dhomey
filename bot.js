7cac4cd06732f04c7eb2c70af671f628767acabd
dhomey <49097128+dhomey@users.noreply.github.com>
2019-03-31 23:11:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,5 +62,44 @@ msg.delete();
 });
 
 
+client.on('message', function(msg) {
+        let verifLevels = ["None", "Low", "Medium", "(╯°□°）╯︵  ┻━┻", "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻"];
+        let region = {
+            "brazil": "Brazil",
+            "eu-central": "Central Europe",
+            "singapore": "Singapore",
+            "Russia": "Russia",
+            "us-central": "U.S. Central",
+            "sydney": "Sydney",
+            "us-east": "U.S. East",
+            "us-south": "U.S. South",
+            "us-west": "U.S. West",
+            "eu-west": "Western Europe",
+            "vip-us-east": "VIP U.S. East",
+            "london": "London",
+            "amsterdam": "Amsterdam",
+            "hongkong": "Hong Kong"
+        };
+      
+          if (msg.content.startsWith(prefix + 'server')) {
+          if (!msg.guild) return message.reply('**Only Servers | :x:**')
+      console.log(`${msg.author.username} Has Ran Server Command`)
+          let embed = new Discord.RichEmbed()
+          .setColor('RANDOM')
+          .setThumbnail(msg.guild.iconURL)
+          .setTitle(`${msg.guild.name}`)
+          .addField('**[❖] Server Name | اسم السيرفر**',`[** __${msg.guild.name}__ **]`,true)
+          .addField('**[❖] OwnerShip | مؤسس السيرفر**',`**${msg.guild.owner}**`,true)
+          .addField('**[❖] Server ID | معرف السيرفر**',`**${msg.guild.id}**`,true)
+          .addField('**[❖] Members Count | عدد الاعضاء**',`[** __${msg.guild.memberCount}__ **]`,true)
+          .addField('**[❖] Verification Level | مستوي الحمايه**',`[** __${verifLevels[msg.guild.verificationLevel]}__** ]`,true)
+          .addField('**[❖] Region | البلد**',`[** __${region[msg.guild.region]}__** ]`,true)
+          .addField('**[❖] Text Channels | رومات كتابيه**',`[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
+          .addField('**[❖] Voice Channels | رومات صوتيه**',`[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
+          .addField('**[❖] Created At | صنع في**',msg.guild.createdAt.toLocaleString())
+          msg.channel.send({embed:embed});
+        }
+      });
+
 
 client.login(process.env.BOT_TOKEN);
