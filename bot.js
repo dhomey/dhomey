94d9455c9c790b0c010f2ce6bb63c59616bf7c1f
dhomey <49097128+dhomey@users.noreply.github.com>
2019-04-01 17:27:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -136,4 +136,22 @@ var ApL = `${Math.round(client.ping)}`
  });
 
 
+client.on('message', message => {
+             if (!message.channel.guild) return;
+     if(message.content =='$members')
+     var IzRo = new Discord.RichEmbed()
+     .setThumbnail(message.author.avatarURL)
+     .setFooter(message.author.username, message.author.avatarURL) 
+     .setTitle('🌷| Members info')
+     .addBlankField(true)
+     .addField('📗| Online ',
+     `${message.guild.members.filter(m=>m.presence.status == 'online').size}`)
+     .addField('📕| DND',`${message.guild.members.filter(m=>m.presence.status == 'dnd').size}`)
+     .addField('📙| idle',`${message.guild.members.filter(m=>m.presence.status == 'idle').size}`)
+     .addField('📓| Offline ',`${message.guild.members.filter(m=>m.presence.status == 'offline').size}`)
+     .addField('Members Count',`${message.guild.memberCount}`)
+     message.channel.send(IzRo);
+   });
+
+
 client.login(process.env.BOT_TOKEN);
