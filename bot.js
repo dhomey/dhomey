20c10a6f6a913162018f931f46ddc35f65a16986
dhomey <49097128+dhomey@users.noreply.github.com>
2019-04-01 16:57:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -99,10 +99,22 @@ client.on("message",async msg => {
   });
 
 
-client.on('message', message => {
-  if(message.content.startsWith(prefix + "ping")) {
-message.channel.send(`MessageTaken: `${Date.now() - message.createdTimestamp}`msn DiscordAPi: `${Math.round(client.ping)}`msnAverage: `${Math.round(client.pings[0])}`ms. `)
-}
+client.on('message',async message => {
+  if(message.content.startsWith(prefix + "server")) {
+    let embed = new Discord.RichEmbed()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setTitle(``${message.guild.name}``)
+    .setThumbnail(message.guild.iconURL)
+    .addField('• iD:', `- ${message.guild.id}`,true)
+    .addField('• Owner:', `- ${message.guild.owner}`, true)
+    .addField('• Channels:', ``#` ${message.guild.channels.filter(a => a.type === 'text').size} - `🎤` ${message.guild.channels.filter(a => a.type === 'voice').size}`, true)
+    .addField('• Members:', ``Count` ${message.guild.memberCount} - `Last` ${Array.from(message.channel.guild.members.values()).sort((a, b) => b.joinedAt - a.joinedAt).map(m => `${m}`).splice(0, 1)}`, true)
+    .addField('• AFK Channel:', `${message.guild.afkChannel || 'None'}`, true)
+    .addField('• Other:', ``Roles` ${message.guild.roles.size} - `Emojis` ${message.guild.emojis.size} `[` ${message.guild.emojis.map(m => m).join(' **|** ')} `]``,true)
+    .addField('• Region:', `${message.guild.region}`, true);
+
+    message.channel.send(embed);
+  }
 });
 
 
