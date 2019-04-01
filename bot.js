fcf219852dca0253e518199483b3ee9951288f8c
dhomey <49097128+dhomey@users.noreply.github.com>
2019-04-01 16:58:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,86 +18,11 @@ client.on('ready', () => {
 
   console.log(`Logged in as ${client.user.tag}!`);
 
-client.user.setGame(`Stats Bot : BETA`,"http://twitch.tv/S-F")
+client.user.setGame(`$help | $invite`,"http://twitch.tv/S-F")
 
 client.user.setStatus("dnd")
 
 });
-
-
-
-client.on('message', message =>{
-  if(message.content.startsWith(prefix + 'stats')){
-  if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **لا تملك صلاحية**');
-  if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return message.reply('❌ **البوت لا يمتلك صلاحية**');
-  message.guild.createChannel(`👑معلومات السيرفر👌:` , 'category')
-  
-    message.guild.createChannel(`"انتظر قليلا` , 'voice').then(time => {
-    time.overwritePermissions(message.guild.id, {
-      CONNECT: false,
-      SPEAK: false
-    });
-  
-  setInterval(() => {
-      time.setName(`${message.guild.memberCount} <== عدد الكل `);
- },1000);
-    });
-
- message.guild.createChannel(`"انتظر قليلا` , 'voice').then(time => {
-  time.overwritePermissions(message.guild.id, {
-    CONNECT: false,
-    SPEAK: false
-  });
-setInterval(() => {
-    time.setName(`${message.guild.members.filter(m =>!m.user.bot).size} <==  عدد الاعضاء `);
-},1500);
-});
-
-message.guild.createChannel(`"انتظر قليلا` , 'voice').then(time => {
-  time.overwritePermissions(message.guild.id, {
-    CONNECT: false,
-    SPEAK: false
-  });
-setInterval(() => {
-    time.setName(`${message.guild.members.filter(m=>m.user.bot).size} <==  عدد البوتات `);
-},2000);
-});
-}
-});
-
-
-client.on("message",async msg => {
-    if(msg.content.startsWith(prefix + "clear")){
-      let args = msg.content.split(" ").slice(1).join(" ");
-      if(!args)  return msg.reply(`**${msg.content} <Number Messages Deleted?>**`)
-      msg.reply("**Are You Sure Of The Deleted Messages?**").then(o => {
-        o.react("✅")
-        .then(()=> o.react('❎'))
-        .then(()=> o.react("✅"))
-        let reaction1 = (reaction,user) => reaction.emoji.name === "✅" && user.id === msg.author.id
-        let reaction2 = (reaction,user) => reaction.emoji.name === "❎" && user.id === msg.author.id
-        let react3 = o.createReactionCollector(reaction1, { time: 12300})
-        let react4 = o.createReactionCollector(reaction2, { time: 12300})
-        react3.on("collect", r => {
-         msg.channel.bulkDelete(args)
-          msg.reply(`**Done Deleted Messages ${args}**`).then(op => {
-          op.delete(1200)
-         o.delete(1200)
-         msg.delete(1200)
-       })
-       react4.on("collect", r => {
-        msg.reply(`**Done Deleted Messages Has Been Cancel**`).then(ob => {
-          ob.delete(1200)
-          o.delete(1200)
-          msg.delete(1200)
-        })
-        })
-      })
-    })
-    
-    }
-  });
-
 
 client.on('message',async message => {
   if(message.content.startsWith(prefix + "server")) {
