53e0251192bb386904d9c06e32a0c2242f176c38
dhomey <49097128+dhomey@users.noreply.github.com>
2019-04-12 14:55:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -694,4 +694,23 @@ client.on('ready', () => {
       });
 
 
+client.on('message', message => {
+    if (message.content === ('.bot')) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .addField('**Bot Ping**🚀 :' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('**Servers**📚 :', [client.guilds.size], true)
+            .addField('**Channels**📝 :' , `[ ${client.channels.size} ]` , true)
+            .addField('**Users**🔮 :' ,`[ ${client.users.size} ]` , true)
+            .addField('**Bot Name**🔰 :' , `[ ${client.user.tag} ]` , true)
+            .addField('**Bot Owner**👑 :' , `! iDhoom#2557` , true)
+            .setFooter(message.author.username, message.author.avatarURL)
+    })
+}
+});
+
+
 client.login(process.env.BOT_TOKEN);
