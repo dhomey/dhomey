e5ad26fe0c5ecda1d6ad7d7243bed912ff5e21e6
dhomey <49097128+dhomey@users.noreply.github.com>
2019-04-20 05:34:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,4 +65,14 @@ client.on('message', function(msg) {
       });
 
 
+client.on('message', message =>{//ping
+if(message.content.startsWith(prefix  +  'ping'))  {
+let start = Date.now(); message.channel.send('pong').then(message => { 
+message.edit(`
+Discord API: ${client.ping.toFixed(0)} ms`);
+  });
+  }
+});
+
+
 client.login(process.env.BOT_TOKEN);
