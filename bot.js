770c4e962422b0a52b1eb2f21692a7d378040acb
dhomey <49097128+dhomey@users.noreply.github.com>
2019-04-20 05:51:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -198,4 +198,28 @@ if(message.content.split(' ')[0] == '$bc') {
 })
 
 
+client.on('message', message => {
+  if (true) {
+if (message.content === '$invite') {
+      message.author.send('  رابط بوتك  |  تفضل ربط البوت     ').catch(e => console.log(e.stack));
+ 
+    }
+   }
+  });
+ 
+ 
+client.on('message', message => {
+     if (message.content === ".invite") {
+     let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#9B59B6")
+  .addField(" Done | تــــم" , " |  تــــم ارســالك في الخــاص")
+     
+     
+     
+  message.channel.sendEmbed(embed);
+    }
+});
+
+
 client.login(process.env.BOT_TOKEN);
