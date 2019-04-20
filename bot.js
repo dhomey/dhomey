70d2552590ad8d66f0442c8b03383e03434bda50
dhomey <49097128+dhomey@users.noreply.github.com>
2019-04-20 22:27:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -129,30 +129,6 @@ client.on('message', async msg =>{
 });//Alpha Codes_LioNDz
 
 
-client.on("message", message => {
-    var prefix = "$";
-            var args = message.content.substring(prefix.length).split(" ");
-            if (message.content.startsWith(prefix + "clear")) {
- if (!args[1]) {
-                                let x5bz1 = new Discord.RichEmbed()
-                                .setDescription("clear <number>")
-                                .setColor("#0000FF")
-                                message.channel.sendEmbed(x5bz1);
-                            } else {
-                            let messagecount = parseInt(args[1]);
-                            message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
-                                                          message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
-                            message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
-                            let x5bz2 = new Discord.RichEmbed()
-                                                            .setColor("#008000")
-                                .setDescription(":white_check_mark: | Delete " + args[1] + " Message!")
-                                                                                        message.delete("..");
-                                message.channel.sendEmbed(x5bz2);
-                            }
-                          }
-});
-
-
 client.on('message', message => {
     if(message.content.startsWith(prefix+'sup')) {
   const embed = new Discord.RichEmbed()
