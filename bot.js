5a67b99dbcd83eece34d9eee9cfeb0383c867f8e
dhomey <49097128+dhomey@users.noreply.github.com>
2019-04-20 05:42:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -107,4 +107,59 @@ client.on("message", message => {
 					}});
 
 
+client.on('message', async msg =>{
+    var prefix = '§';//هنا البريفيكس
+    if (msg.author.bot) return undefined;
+    if (!msg.content.startsWith(prefix)) return undefined;
+   
+    let args = msg.content.split(' ');
+ 
+    let command = msg.content.toLowerCase().split(" ")[0];
+    command = command.slice(prefix.length)
+ 
+    if(command === `ping`) {//هنا الكوماند
+    let embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setTitle("Your Ping!!")
+    .setDescription(`${client.ping} ms`)
+    .setFooter(`${msg.author.tag}`);
+    msg.delete().catch(O_o=>{})
+    msg.channel.send(embed);
+    }
+});//Alpha Codes_LioNDz
+
+
+client.on("message",async msg => {
+    if(msg.content.startsWith(prefix + "clear")){
+      let args = msg.content.split(" ").slice(1).join(" ");
+      if(!args)  return msg.reply(`**${msg.content} <Number Messages Deleted?>**`)
+      msg.reply("**Are You Sure Of The Deleted Messages?**").then(o => {
+        o.react("✅")
+        .then(()=> o.react('❎'))
+        .then(()=> o.react("✅"))
+        let reaction1 = (reaction,user) => reaction.emoji.name === "✅" && user.id === msg.author.id
+        let reaction2 = (reaction,user) => reaction.emoji.name === "❎" && user.id === msg.author.id
+        let react3 = o.createReactionCollector(reaction1, { time: 12300})
+        let react4 = o.createReactionCollector(reaction2, { time: 12300})
+        react3.on("collect", r => {
+         msg.channel.bulkDelete(args)
+          msg.reply(`**Done Deleted Messages ${args}**`).then(op => {
+          op.delete(1200)
+         o.delete(1200)
+         msg.delete(1200)
+       })
+       react4.on("collect", r => {
+        msg.reply(`**Done Deleted Messages Has Been Cancel**`).then(ob => {
+          ob.delete(1200)
+          o.delete(1200)
+          msg.delete(1200)
+        })
+        })
+      })
+    })
+    
+    }
+  });
+
+
 client.login(process.env.BOT_TOKEN);
